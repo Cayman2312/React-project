7333bd41c84e6dc2dd2fdf4dc0a983e5ed4ebebe
M.Studitskiy <cayman2312@gmail.com>
2020-04-17 13:30:39 +0300

    Варианты создания компонта (функция, стрелочная функция, export default)

---
+++ src/Car/Car.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// function Car() {
+//   return <h2>This is car component</h2>;
+// }
+
+// const Car = () => (
+//   <div>
+//     This is car component
+//     <strong> Test</strong>
+//   </div>
+// );
+
+export default () => (
+  <div>
+    This is car component
+    <strong> Test</strong>
+  </div>
+);
