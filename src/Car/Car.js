bb92924ef94c560bc74c5a407e223c04370ed165
M.Studitskiy <cayman2312@gmail.com>
2020-04-18 19:10:26 +0300

    создание функции-обертки (функция высшего порядка) - сложно для понимания

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,33 +1,10 @@
 import React from 'react';
 // import Radium from 'radium';
 import './Car.scss';
+import withClass from '../hoc/withClass';
 
 class Car extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate', nextProps, nextState);
-    return nextProps.name.trim() !== this.props.name.trim();
-  }
-
-  // static getDerivedStateFromProps(nextProps, prevState) {
-  //   console.log('getDerivedStateFromProps', nextProps, prevState);
-  //   return prevState;
-  // }
-
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
-
-  // getSnapshotBeforeUpdate() {
-  //   console.log('getSnapshotBeforeUpdate');
-  // }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-  }
-
   render() {
-    console.log('Car render');
-
     const inputClasses = ['input'];
 
     if (this.props.name !== '') {
@@ -39,18 +16,8 @@ class Car extends React.Component {
       inputClasses.push('bold');
     }
 
-    const style = {
-      border: '1px solid #ccc',
-      boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-      ':hover': {
-        border: '1px solid #aaa',
-        boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
-        cursor: 'pointer',
-      },
-    };
-
     return (
-      <div className="Car" style={style}>
+      <React.Fragment>
         <h3>Car name: {this.props.name}</h3>
         <p>
           Year: <strong>{this.props.year}</strong>
@@ -62,9 +29,9 @@ class Car extends React.Component {
           className={inputClasses.join(' ')}
         />
         <button onClick={this.props.onDelete}>Delete</button>
-      </div>
+      </React.Fragment>
     );
   }
 }
 
-export default Car;
+export default withClass(Car, 'Car');
